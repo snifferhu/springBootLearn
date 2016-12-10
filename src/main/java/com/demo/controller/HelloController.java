fe6a209b79db497f46bc214fee6d7f493439e0c2
sniff <sniff@globlabestsoft.cn>
2016-12-10 11:17:09 +0800

    feat(getMapping): 1.add getMapping controller

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -3,11 +3,13 @@ package com.demo.controller;
 /**
  * Created by Sniff on 2016/11/2.
  */
-
 import com.demo.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -21,4 +23,9 @@ public class HelloController {
         return router.get("GET").doSomeThing();
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello world.";
+    }
 }
