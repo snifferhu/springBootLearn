35aee53cdc930890b305cac30c660939f1f286bd
julien <julien.123.com>
2017-03-18 15:16:51 +0800

    feat(forgot): 1.

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaClient;
 
 /**
  * Created by Sniff on 2016/10/29.
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
