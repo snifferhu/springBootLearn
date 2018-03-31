b80a6bbfee2aad8f71b069bf1c3cdd19b4a619bb
snifferhu <huyong@zhai.me>
2018-03-31 10:49:09 +0800

    feat(feign-hystrix): 1. add feign-hystrix module

--- feign-hystrix/src/main/java/com/example/demo/FeignApplication.java
+++ feign-hystrix/src/main/java/com/example/demo/FeignApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -15,23 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @RestController
 @EnableFeignClients
+@EnableCircuitBreaker
 public class FeignApplication {
+
 	@Autowired
-	Simple client;
+	SimpleService service;
 
 	@RequestMapping("/")
 	public String hello() {
-		return client.home();
+		return service.home();
 	}
 
 	@RequestMapping("/add")
 	public String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
-		return client.add(a,b);
+		return service.add(a,b);
 	}
 
 	@RequestMapping("/greeting")
 	public String greet() {
-		return client.greet();
+		return service.greet();
 	}
 
 	public static void main(String[] args) {
