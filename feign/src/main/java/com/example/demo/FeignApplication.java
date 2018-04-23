8150ef1321d2778c215c6d4f6c8c72bef4dd1b09
snifferhu <huyong@zhai.me>
2018-04-23 21:02:34 +0800

    feat(feign): 1.add feign  eureka

--- feign/src/main/java/com/example/demo/FeignApplication.java
+++ feign/src/main/java/com/example/demo/FeignApplication.java
@@ -1,53 +1,39 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 @EnableFeignClients
 public class FeignApplication {
-	@Autowired
-	Simple client;
-
-	@RequestMapping("/")
-	public String hello() {
-		return client.home();
-	}
-
-	@RequestMapping("/add")
-	public String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
-		return client.add(a,b);
-	}
-
-	@RequestMapping("/greeting")
-	public String greet() {
-		return client.greet();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeignApplication.class, args);
 	}
 
+
+	static class HystrixClientFallback implements SimpleClient {
+
+		@Override
+		public String home() {
+			return "oops";
+		}
+
+		@Override
+		public String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
+			return "oops";
+		}
+
+		@Override
+		public String greet() {
+			return "oops";
+		}
+	}
+
 }
 
-@FeignClient("simple")
-interface Simple {
-    @GetMapping(value = "/")
-    String home();
 
-    @GetMapping(value = "/add")
-    String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
-
-    @GetMapping(value = "/greeting")
-    String greet();
-}
